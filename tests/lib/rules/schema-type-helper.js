3d7328d5d0a16c63c43bd85bc11caea2833739a4
Alexander Liu <a@alexanderliu.com>
2023-06-23 20:55:53 -0400

    feat: schema-type-helper

---
+++ tests/lib/rules/schema-type-helper.js
@@ -0,0 +1,34 @@
+/**
+ * @fileoverview Enforce "defineType" helper functions for Sanity schemas.
+ * @author Alexander Liu
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/schema-type-helper"),
+  RuleTester = require("eslint").RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+ruleTester.run("schema-type-helper", rule, {
+  valid: [
+    'defineType({ name: "product", type: "document", title: "Product", fields: [] });',
+    'var notSchema = { name: "product", type: "not a type" };',
+    'var notSchema = { a: "a", b: "b" };',
+  ],
+
+  invalid: [
+    {
+      code: 'var product = { name: "product", type: "document", title: "Product", fields: [] };',
+      output:
+        'var product = defineType({ name: "product", type: "document", title: "Product", fields: [] });',
+      errors: [{ messageId: "missingHelperFunction" }],
+    },
+  ],
+});
